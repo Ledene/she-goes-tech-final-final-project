5e23670fcd84597c930d0e8ebf27b573cbc8bd6b
Inese Krole <inese.krole@gmail.com>
2022-04-01 20:32:15 +0300

    Added mail icon in Contacts; added drop-down menu in contact form; put php code in separate file, now success submit message work ok, and redirects user back to index.html page

--- get_response.php
+++ get_response.php
@@ -3,20 +3,21 @@ $con = new mysqli("localhost", "root", "", "she-goes-tech-final-project");
 if ($con->connect_error)
     die("Connection error" . $con->connect_error);
 
-	$sql = "INSERT INTO contacts (name, email, phone, message) VALUES ('{$_POST["name"]}','{$_POST["email"]}','{$_POST["phone"]}', '{$_POST["message"]}')";
-	
-    if(mysqli_query($con, $sql)){
-        echo "<h3>data stored in a database successfully." 
-            . " Please browse your localhost php my admin" 
-            . " to view the updated data</h3>"; 
+  
+// if (isset($_POST['submit']))
 
-        echo nl2br("\n'{$_POST["name"]}'\n,\n'{$_POST["email"]}'\n,\n'{$_POST["phone"]}'\n, \n'{$_POST["message"]}'\n");
-    } else{
-        echo "ERROR: Hush! Sorry $sql. " 
-            . mysqli_error($con);
+	$sql = "INSERT INTO contacts (topic, name, email, phone, message) VALUES ('{$_POST["topic"]}','{$_POST["name"]}','{$_POST["email"]}','{$_POST["phone"]}', '{$_POST["message"]}')";
+
+       if(mysqli_query($con, $sql)){
+        echo '<script type="text/javascript">alert("submit success");</script>';
+       
+    }   
+     else{
+        echo '<script type="text/javascript">alert("submission error, please try again");</script>';
     }
       
     // Close connection
     mysqli_close($con);
     ?>
+
 
