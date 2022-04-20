d18c7b15a9955f6a79290e61087e00d8cd71678e
Inese Krole <inese.krole@gmail.com>
2022-04-20 14:04:49 +0300

    cleared the code, changed submit success messsage text

--- index.php
+++ index.php
@@ -6,15 +6,13 @@ if ($con->connect_error)
 	$sql = "INSERT INTO contacts (topic, name, email, phone, message) VALUES ('{$_POST["topic"]}','{$_POST["name"]}','{$_POST["email"]}','{$_POST["phone"]}', '{$_POST["message"]}')";
 
        if(mysqli_query($con, $sql)){
-        echo '<script type="text/javascript">alert("submit success");</script>';
+        echo '<script type="text/javascript">alert("Thank you for your request, we will get back to you as soon as possible!");</script>';
        
     }   
      else{
         echo '<script type="text/javascript">alert("submission error, please try again");</script>';
     }
    
-    
-    // Close connection
     mysqli_close($con);
     echo "<META http-equiv=\"refresh\" content=\"0;URL=http://localhost/she-goes-tech-final-final-project/index.html\">";
     ?>
